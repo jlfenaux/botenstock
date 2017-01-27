823b3944cf4a0ec5294953b2c87b009945af7982
Jean-Loup Fenaux <jlfenaux@ahotu.com>
2017-01-27 15:13:02 +0100

    Ajoute la homepage et teste une nouvelle couleur

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :bots
-  get 'annuaire/:platform/:category/(:keywords)' => 'bots#index', as: 'bot_directory', defaults: {category: 'toutes_les_catégories', platform: 'toutes_les_plateformes'}
-  root to: 'bots#index'
+  get 'annuaire/:platform/:category/(:keywords)' => 'bots#index', as: 'bot_directory', defaults: {category: 'toutes_les_catégories', platform: 'toutes_les_plateformes', locale:'fr'}
+
+  root to: 'homepage#index'
 
 end
