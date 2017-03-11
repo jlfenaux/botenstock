e351c84d3d80541a99636ba53c078f7fb7059fbf
Jean-Loup Fenaux <jlfenaux@ahotu.com>
2017-03-11 09:56:49 +0100

    le blog est affiché pour tous

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,6 +1,4 @@
 class BlogController < ApplicationController
-  before_action :authenticate_user!
-  before_action :check_if_admin
   def index
     @posts = Post.published.language(I18n.locale).order('published_at desc').paginate(:page => params[:page], :per_page => 20)
   end
