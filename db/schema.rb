5a9f1dbf39b29cb740e88245b80a39fae73e2df0
Jean-Loup Fenaux <jlfenaux@ahotu.com>
2017-03-11 16:13:24 +0100

    modifie la BDD pour confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311085822) do
+ActiveRecord::Schema.define(version: 20170311150525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,11 @@ ActiveRecord::Schema.define(version: 20170311085822) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "roles",                  default: [],              array: true
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
