4dc625d229659615802d2ee0fe709b55a1c9d9f1
Jean-Loup Fenaux <jlfenaux@ahotu.com>
2017-03-16 17:51:37 +0100

    config.action_mailer.asset_host pour la prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "botenstock_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { :host => 'botsforchatting.com' }
+  config.action_mailer.asset_host = "https://botsforchatting.com"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
