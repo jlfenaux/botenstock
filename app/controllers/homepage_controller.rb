dba49c58eca2066109797df7a14e6bd95b9a4a01
Jean-Loup Fenaux <jlfenaux@ahotu.com>
2017-06-06 09:34:52 +0200

    alternate hreflang utilise les url au lieu des path

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,8 +2,8 @@ class HomepageController < ApplicationController
   def index
     set_meta_tags description: I18n.locale == :fr ? "Botsforchatting est un annuaire de bots indépendant recensant et testant tous les bots, sur toutes les plates-formes." : "Botsforchatting is an independent bots directory listing and testing all bots on all platforms."
     @translated_links = {}
-    @translated_links[:en] = root_en_path if I18n.locale != :en
-    @translated_links[:fr] = root_fr_path if I18n.locale != :fr
+    @translated_links[:en] = root_en_url if I18n.locale != :en
+    @translated_links[:fr] = root_fr_url if I18n.locale != :fr
     set_meta_tags alternate: @translated_links
   end
 
